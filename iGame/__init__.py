0a5f0c1e53b2376859571b7d2d3050a7a6f45092
Justin Branch <justincarltonbranch@gmail.com>
2025-01-03 18:02:46 -0500

    debugging imports

--- iGame/__init__.py
+++ iGame/__init__.py
@@ -37,7 +37,7 @@ def create_app(config_name: str):
     from .main import main as main_bp
     print(type(main_bp))
     from .auth import auth as auth_bp
-    print(f'auth obj type: {type(auth_bp)}')
+    print(type(auth_bp))
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
 
