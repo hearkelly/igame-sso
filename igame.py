0f061a703c979692cce103da2de3498d6b3296f6
Justin Branch <justincarltonbranch@gmail.com>
2025-01-25 12:34:56 -0500

    ssl

--- igame.py
+++ igame.py
@@ -20,13 +20,6 @@ from iGame import create_app
 # with factory pattern, app has no explicit context
 app = create_app(os.environ.get('FLASK_CONFIG') or 'default')
 
-@app.before_request
-def force_https():
-    print("enforcing")
-    if not request.is_secure and not app.debug:
-        url = request.url.replace("http://", "https://", 1)
-        return redirect(url, code=301)
-
 # @app.before_request
 # def log_request_info():
 #     print(f"Request headers: {request.headers}")
