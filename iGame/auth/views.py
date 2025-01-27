7a10d49efc19de8e65a503a3a5a2a9a53e63d355
Justin Branch <justincarltonbranch@gmail.com>
2025-01-27 18:17:45 -0500

    tests

--- iGame/auth/views.py
+++ iGame/auth/views.py
@@ -134,10 +134,11 @@ def register():
 
 @auth.route('/_games/<_id>')
 def get_users(_id=0):
-    rq = db.session.query(Game.game_id, Game.rating).filter(
-        and_(Game.user_id == _id, Game.likes == True)).all()
-    bag_count = db.session.query(func.count(Game)).filter(
-        and_(Game.user_id == _id, Game.likes == True)).scalar()
-    rq_scalars = db.session.query(Game.game_id, Game.rating).filter(
-        and_(Game.user_id == _id, Game.likes == True)).scalars()
-    return render_template('t_.html',data=[rq,bag_count,rq_scalars])
+    # rq = db.session.query(Game.game_id, Game.rating).filter(
+    #     and_(Game.user_id == _id, Game.likes == True)).all()
+    rq_scalars = db.session.execute(db.select(Game.game_id)).scalars()
+    # bag_count = db.session.query(func.count(Game)).filter(
+    #     and_(Game.user_id == _id, Game.likes == True)).scalar()
+    # rq_scalars = db.session.query(Game.game_id, Game.rating).filter(
+    #     and_(Game.user_id == _id, Game.likes == True)).scalars()
+    return render_template('t_.html',data=[rq_scalars])
