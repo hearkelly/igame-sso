c81116c0bb950adb88aee25b6099c83c19851db9
Justin Branch <justincarltonbranch@gmail.com>
2025-01-27 19:49:28 -0500

    sql

--- iGame/auth/views.py
+++ iGame/auth/views.py
@@ -128,7 +128,7 @@ def get_users(_id=0):
     # rq = db.session.query(Game.game_id, Game.rating).filter(
     #     and_(Game.user_id == _id, Game.likes == True)).all()
     rq_scalars = db.session.execute(db.select(Game.game_id).where(Game.user_id == _id)).scalars()
-    print(rq_scalars)
+    print(list(rq_scalars))
     # bag_count = db.session.query(func.count(Game)).filter(
     #     and_(Game.user_id == _id, Game.likes == True)).scalar()
     # rq_scalars = db.session.query(Game.game_id, Game.rating).filter(
